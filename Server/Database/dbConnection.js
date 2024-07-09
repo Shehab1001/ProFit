0a318cc6bdf80258dfbe45e5a03bf486baf8218f
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-07-09 04:18:56 +0300

    db connection

--- Server/Database/dbConnection.js
+++ Server/Database/dbConnection.js
@@ -1,3 +1,12 @@
 import mongoose from "mongoose";
 
-
+export let dbConnection = () => 
+{
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=> {
+        console.log("DB Connected Successfully");
+    })
+    .catch((error)=>{
+        console.log("DB Failed to Connect");
+    })
+}
