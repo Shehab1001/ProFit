42c57240429a5b1e3fdb1457dd0f8ccb267275e1
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-07-10 23:17:29 +0300

    client transformation

--- Server/Database/models/clientTransformations.js
+++ Server/Database/models/clientTransformations.js
@@ -27,31 +27,6 @@ const clientTransformationSchema = new Schema(
   },
   { timestamps: true }
 );
-// const BASE_FILE_URL = "http://localhost:4000/";
-
-// clientTransformationSchema.post("init", function (doc) {
-//   if (doc) {
-//     if (
-//       doc.beforeImage &&
-//       doc.beforeImage.length > 0 &&
-//       !doc.beforeImage.startsWith(BASE_FILE_URL)
-//     ) {
-//       const pathParts = doc.beforeImage.split("\\").slice(-4);
-//       doc.beforeImage =
-//         BASE_FILE_URL + "uploads/" + pathParts.join("/").replace(/\s/g, "%20");
-//     }
-
-//     if (
-//       doc.afterImage &&
-//       doc.afterImage.length > 0 &&
-//       !doc.afterImage.startsWith(BASE_FILE_URL)
-//     ) {
-//       const pathParts = doc.afterImage.split("\\").slice(-4);
-//       doc.afterImage =
-//         BASE_FILE_URL + "uploads/" + pathParts.join("/").replace(/\s/g, "%20");
-//     }
-//   }
-// });
 
 export const ClientTransformationModel = model(
   "ClientTransformation",
